dab5a0eeed15da6ac1fa4a3b4b61c97e03985ddc
pdefusco <pauldefusco@gmail.com>
2022-11-14 18:05:28 -0800

    pushing script

--- cde_job_move_file.py
+++ cde_job_move_file.py
@@ -1,32 +1,34 @@
 #https://pypi.org/project/azure-storage-blob/
-
+import os
 from azure.storage.blob import BlobServiceClient
 from azure.storage.blob import ContainerClient
 from azure.storage.blob import BlobClient
 
 #Enter your connection string here:
 connection_string = "DefaultEndpointsProtocol=https;AccountName=xxxx;AccountKey=xxxx;EndpointSuffix=core.windows.net"
-service = BlobServiceClient.from_connection_string(conn_str=connection_string)
+file_path = "myfile_1.py"
+cde_resource_path = "/app/mount"
 
-file_name = "myfile_1.py"
-cde_resource_path =
+upload_file_path = os.path.join(cde_resource_path, file_path)
 
-#Create a container
-source_container_name = "my_source_container"
-container_client = ContainerClient.from_connection_string(conn_str=connection_string, container_name=source_container_name)
-container_client.create_container()
+blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
 
-#Upload a file
-blob = BlobClient.from_connection_string(conn_str=connection_string, container_name="my_container", blob_name="my_blob")
+source_container_name = "source_container"
+target_container_name = "target_container"
 
-with open("/app/mount/myfile_1.py", "rb") as data:
-    blob.upload_blob(data)
+container_client = blob_service_client.create_container(source_container_name)
+container_client = blob_service_client.create_container(target_container_name)
 
-#Create other container
-target_container_name = "my_target_container"
-container_client = ContainerClient.from_connection_string(conn_str=connection_string, container_name=target_container_name)
-container_client.create_container()
+blob_client = blob_service_client.get_blob_client(container=source_container_name, blob=file_path)
+print("\nUploading to Azure Storage as blob:\n\t" + file_path)
+with open(upload_file_path, "rb") as data:
+    blob_client.upload_blob(data)
 
-#Target
-copied_blob = blob_service_client.get_blob_client(target_container_name, file_name)
+
+account_name = "azure_account_name"
+# Source
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+source_blob = (f"https://{account_name}.blob.core.windows.net/{source_container_name}/{file_path}")
+# Target
+copied_blob = blob_service_client.get_blob_client(target_container_name, upload_file_path)
 copied_blob.start_copy_from_url(source_blob)
